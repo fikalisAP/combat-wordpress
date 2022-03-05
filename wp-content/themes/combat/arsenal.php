0dbc658c818b5d853f8c053e1e49fd8ca21c24aa
yoolper <yoolper@yandex.ru>
2022-03-05 16:24:58 +0500

    hello

---
+++ wp-content/themes/combat/arsenal.php
@@ -0,0 +1,109 @@
+<?php
+/*
+    Template Name: arsenal
+*/
+?>
+<?
+get_header();
+?>
+    <section class="arsenal">
+        <div class="container">
+            <h2 class="title">Арсенал</h2>
+            <div class="arsenal__block">
+                <div class="arsenal__inner">
+                    <h3 class="services-pages__title arsenal-title">Eraser</h3>
+                    <div class="arsenal__image">
+                        <img src="images/eraser.jpg" alt="eraser">
+                    </div>
+                    <div class="arsenal__text">
+                        <p>ERASER- это уникальный игровой комплект, созданный специально для лазертага. На создание корпуса нас вдохновил пистолет-пулемет MP7 PDW немецкой фирмы Heckler & Koch. Легкий, прочный, компактный и удобный ERASER востребован во многих направлениях лазертага. Благодаря небольшим размерам и весу (0,58 кг) ERASER идеально подходит для большинства лазертаг-клубов, ведь примерно 50% их аудитории - это дети.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="arsenal__block">
+                <div class="arsenal__inner">
+                    <h3 class="services-pages__title arsenal-title">Destroyer</h3>
+                    <div class="arsenal__image">
+                        <img src="images/destroyer.jpg" alt="destroyer">
+                    </div>
+                    <div class="arsenal__text">
+                        <p>DESTROYER - уникальный игровой комплект, дизайн которого был разработан инженерами специально для игры в лазертаг. Это один из первых макетов, с производства которых началось становление компании LASERWAR как разработчика и производителя специализированного оборудования. Корпус выполнен из ударопрочного пластика. Благодаря своим характеристикам является одним из наилучших решений для детей и женщин.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="arsenal__block">
+                <div class="arsenal__inner">
+                    <h3 class="services-pages__title arsenal-title">Ак-12 Хищник</h3>
+                    <div class="arsenal__image">
+                        <img src="images/ak12.jpg" alt="ak12">
+                    </div>
+                    <div class="arsenal__text">
+                        <p>АК-12LT «Хищник» возглавляет линейку оборудования собственной разработки LASERWAR.  Инженеры изменили конструкцию корпуса так, чтобы получился автомат для лазертага, разработали пресс-форму и начали производство корпусов на базе предприятия. АК-12 стал первым тагером, который специалисты LASERWAR полностью изготовили своими силами. Сейчас этот продукт является флагманским на рынке оборудования для внеаренного лазертага.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="arsenal__block">
+                <div class="arsenal__inner">
+                    <h3 class="services-pages__title arsenal-title">МР-512 «СНАЙПЕР»</h3>
+                    <div class="arsenal__image">
+                        <img src="images/mp512.jpg" alt="mp512">
+                    </div>
+                    <div class="arsenal__text">
+                        <p>МР-512 «СНАЙПЕР» в своей основе пневматическая винтовка. Поэтому про боевые аналоги говорить не приходится. Но если говорить о нише пневматических винтовок, то здесь «Мурка», а именно так её называют обладатели, занимает особое место.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="arsenal__block">
+                <div class="arsenal__inner">
+                    <h3 class="services-pages__title arsenal-title">Аптечка</h3>
+                    <div class="arsenal__image">
+                        <img src="images/apte4ka.jpg" alt="apte4ka">
+                    </div>
+                    <div class="arsenal__text">
+                        <p>Аптечка называется базовой, поскольку её устанавливают на базах противоборствующих команд. Она предназначена для пополнения «здоровья», «боеприпасов» и «возрождения» игроков.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="arsenal__block">
+                <div class="arsenal__inner">
+                    <h3 class="services-pages__title arsenal-title">Бомба</h3>
+                    <div class="arsenal__image">
+                        <img src="images/plant.jpg" alt="bomb">
+                    </div>
+                    <div class="arsenal__text">
+                        <p>Модернизированная версия лазертаг-бомбы. «Кратер» борется с читерами, оснащен семисегментным индикатором и упакован в ударопрочный корпус.
+
+                            В «Кратере» мы использовали контрастные цвета, чтобы подчеркнуть важные элементы и не отвлекать игрока второстепенными деталями. Но при этом девайс интересно рассматривать: фирменный шрифт, маркировка изделия и логотип LASERWAR добавляют очков к общему впечатлению.
+                            
+                            Устройство выполнено в черном пластиковом корпусе. Благодаря специальным проушинам на задней крышке изделие можно зафиксировать на стене, что удобно и практично при играх в помещении</p>
+                    </div>
+                </div>
+            </div>
+            <div class="arsenal__block">
+                <div class="arsenal__inner">
+                    <h3 class="services-pages__title arsenal-title">Контрольная точка</h3>
+                    <div class="arsenal__image">
+                        <img src="images/final-point.jpg" alt="final-point">
+                    </div>
+                    <div class="arsenal__text">
+                        <p>К лазертаг-оборудованию с каждым годом предъявляется все больше и больше требований. Игрокам нужны действительно умные устройства: электроника должна безошибочно обрабатывать все игровые события в автоматическом режиме. А кроме этого, от каждого девайса ждут стильного дизайна, громкой озвучки, яркой индикации и надежности в использовании. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="arsenal__block">
+                <div class="arsenal__inner">
+                    <h3 class="services-pages__title arsenal-title">Командный пункт</h3>
+                    <div class="arsenal__image">
+                        <img src="images/command_punkt.jpg" alt="final-point">
+                    </div>
+                    <div class="arsenal__text">
+                        <p>Командный пункт создавался как универсальный девайс, подходящий для использования абсолютно во всех сценариях и заменяющий множество дополнительных устройств.Командный пункт поддерживает три основных сценария: «Базовая аптечка», «Захват базы», «Противостояние». В каждом из них пользователю доступно множество настроек. Благодаря этому без «Командного пункта» не обходится ни одна крупная сценарная игра.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?
+get_footer();
+wp_footer();
+?>
