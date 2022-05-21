f574f631aa1dff0a27878c375abb0072c2edd04f
fikalisAP <ivan.sherbetov@yandex.ru>
2022-05-21 13:28:46 +0500

    sss1

--- wp-content/themes/combat/assets/js/main.js
+++ wp-content/themes/combat/assets/js/main.js
@@ -1,7 +1,10 @@
 
 $(function () {
     $('.menu__btn').on('click', function () {
+        $('.menu__btn').toggleClass('active__burger');
         $('.menu__items').toggleClass('active');
+        $('body').toggleClass('test');
+        $('.logo').toggleClass('test2')
     });
     $('.reviews__slider').slick({
         dots: true,
